47fe3f6ecedbb9261c18a54124fb180f54145919
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-31 11:50:16 -0500

    version

--- kitchingroup-9.el
+++ kitchingroup-9.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
 ;; Keywords:
-
+;; Version: 0
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
